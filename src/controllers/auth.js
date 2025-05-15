176601ea75a8a509a65a40530360871a8f781adb
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-05-15 10:25:36 +0300

    Save user to database with signup

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 router.get('/sign-up', (req, res) => {
   res.render('auth/sign-up.ejs')
+})
+
+router.post('/sign-up', async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username })
+  if (userInDatabase) {
+    return res.send('<h1>Username already taken</h1>')
+  }
+
+  if (req.body.confirmPassword !== req.body.password) {
+    return res.send('<h1>Enter matching password and confirm password</h1>')
+  }
+  req.body.commercial = req.body.commercial === 'on'
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+  req.body.password = hashedPassword
+  const user = await User.create(req.body)
+  res.redirect('/')
 })
 
 module.exports = router
