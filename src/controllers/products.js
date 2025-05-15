c5367509c27d620cc8169b29a5ab8acec929c904
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-05-15 14:56:48 +0300

    Make products/new GET and POST routs

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,5 +4,17 @@ const bcrypt = require('bcrypt')
 const protected = require('../middleware/protected')
 const Product = require('../models/product')
 const Category = require('../models/category')
+const session = require('express-session')
+
+router.use(protected)
+router.get('/new', (req, res) => {
+  res.render('products/new.ejs')
+})
+
+router.post('/new', async (req, res) => {
+  req.body.userId = req.session.user._id
+  await Product.create(req.body)
+  res.redirect('/')
+})
 
 module.exports = router
