f0d33f5610eb7527c032a0f45091e20fceb55f01
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-05-17 23:03:53 +0300

    Make edit car routes

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const protected = require('../middleware/protected')
 const Product = require('../models/product')
+const User = require('../models/user')
 const Category = require('../models/category')
 const session = require('express-session')
 
@@ -23,7 +24,22 @@ router.post('/new', protected, async (req, res) => {
 
 router.get('/:productId', async (req, res) => {
   const foundProduct = await Product.findById(req.params.productId)
-  res.render('products/show.ejs', { product: foundProduct })
+  if (req.session.user) {
+    const foundUser = await User.findById(req.session.user._id)
+    res.render('products/show.ejs', { product: foundProduct, user: foundUser })
+  } else {
+    res.render('products/show.ejs', { product: foundProduct, user: null })
+  }
+})
+
+router.get('/:productId/edit', async (req, res) => {
+  const foundProduct = await Product.findById(req.params.productId)
+  res.render('products/edit.ejs', { product: foundProduct })
+})
+
+router.put('/:productId', async (req, res) => {
+  await Product.findByIdAndUpdate(req.params.productId, req.body)
+  res.redirect(`/products/${req.params.productId}`)
 })
 
 module.exports = router
