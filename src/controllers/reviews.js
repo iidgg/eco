7b2e1e493f72e156138c9bf4937eefde16575807
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-05-18 11:26:13 +0300

    Add review form and post route

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -4,5 +4,10 @@ const protected = require('../middleware/protected')
 const Review = require('../models/review')
 router.use(protected)
 
-
+router.post('/:productId', async (req, res) => {
+  req.body.user_id = req.session.user._id
+  req.body.product_id = req.params.productId
+  await Review.create(req.body)
+  res.redirect(`/products/${req.params.productId}`)
+})
 module.exports = router
