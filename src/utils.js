5757c9525bed626d1bff2ee49abf7962f2c5646c
iidgg <ello@iduu.net>
2025-05-18 15:04:43 +0300

    fix: :)

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,15 @@ const updateSession = async (req, data) => {
   const session = req.session
 
   if (user) {
+    if (!req.session.user) {
+      req.session.user = {}
+    }
+
     req.session.user = {
-      _id: user._id ?? session.user._id,
-      avatar: user.avatar ?? session.user._id,
-      username: user.username ?? session.user._id,
-      firstName: user.first_name ?? session.user._id
+      _id: user._id || session.user._id,
+      avatar: user.avatar || session.user.avatar,
+      username: user.username || session.user.username,
+      firstName: user.first_name || session.user.firstName
     }
   }
 
