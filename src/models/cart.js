062b95bcfa48a4f308976f25abfc5aa02ed6b9aa
iidgg <ello@iduu.net>
2025-05-21 08:22:46 +0300

    Cart & Orders

---
+++ src/models/cart.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [
+    {
+      itemId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: [0, 'Cannot buy less than 0']
+      }
+    }
+  ]
+})
+
+const Cart = mongoose.model('Cart', schema)
+module.exports = Cart
